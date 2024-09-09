cc4d6bb48ee5b15d7fa3118052d420183eb89b42
Samylaneres <samyaneres13@gmail.com>
2024-09-09 10:01:00 -0300

    valor em minutos

---
+++ ativ05.py
@@ -0,0 +1,8 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+Min = int(input("seu valor em minutos"))
+
+Seg = int(Min * 60)
+
+print(F"Seu valor em segundos é{Seg}")
+
+
